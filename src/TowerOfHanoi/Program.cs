28d61fd89a4e492f4a52b3e4fff28e3d54b387dc
ALEX MARTINEZ <martineza79@outlook.com>
2017-05-15 19:38:48 -0500

    Add project files.

---
+++ src/TowerOfHanoi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TowerOfHanoi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            char startPeg = 'A';
+            char endPeg = 'C';
+            char tempPeg = 'B';
+            int totalDisks = 3;
+
+            solveTowers(totalDisks, startPeg, endPeg, tempPeg);
+        }
+
+        private static void solveTowers(int n, char startPeg, char endPeg, char tempPeg)
+        {
+            if (n > 0)
+            {
+                solveTowers(n - 1, startPeg, tempPeg, endPeg);
+                Console.WriteLine("Move disk from " + startPeg + " to " + endPeg);
+                solveTowers(n - 1, tempPeg, endPeg, startPeg);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/TowerOfHanoi/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("TowerOfHanoi")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f2f5f15a-083f-4517-9e6d-d363acacaae0")]
